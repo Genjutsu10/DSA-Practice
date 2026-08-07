913adb3dadb57afb5f9b7d075aa3da8ab68540be
asadp10 <asadpathan01010@gmail.com>
2026-08-07 17:16:24 +0530

    Initial DSA practice

---
+++ 01_Patterns/patterns.cpp
@@ -0,0 +1,126 @@
+//! Question 7
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int i,n=5;
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<n-i-1;j++){
+//             cout<<" ";
+//         } 
+//         for(int j =0;j<2*i-1;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+//     return 0; 
+// } 
+
+
+
+
+//! Question 8
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n=5;
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<i;j++){
+//             cout<<" ";
+//         }
+//         for(int j=0;j<2*n-2*i-1;j++){
+//             cout<<"*";
+//         }
+//     cout<<endl;
+//     }
+// return 0;
+// }
+
+
+
+
+//! Question 9
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i,j,n=5;
+//
+//         for(int i=0;i<n;i++){
+//         for(int j=0;j<n-i-1;j++){
+//             cout<<" ";
+//         } 
+//         for(int j =0;j<2*i-1;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<i;j++){
+//             cout<<" ";
+//         }
+//         for(int j=0;j<2*n-2*i-1;j++){
+//             cout<<"*";
+//         }
+//     cout<<endl;
+//     }
+// return 0;
+// }  
+
+
+
+//! Question 10
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n=5;
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<i+1;j++){
+//             cout<<"*";
+//         }
+//     cout<<endl;
+//     }
+//     for(int i=0;i<n-1;i++){
+//         for(int j=0;j<n-i-1;j++){
+//             cout<<"*";
+//         }
+//     cout<<endl;
+//     }
+// return 0;
+// }  
+
+
+
+//! Question 11 
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n,i,d,j;
+    n=7;
+
+    for(i=0;i<n;i++){
+        for(j=0;j<=i;j++){
+            d=i-j;
+            if(d%2 == 0){
+                cout<<"1";
+            }else{
+                cout<<"0";
+            }
+
+        }
+    cout<<endl;
+    }
+}  
+
+
+// //!  Question 12
+
+// #include<iostream>
+// using namespace 
+
+
+
+
