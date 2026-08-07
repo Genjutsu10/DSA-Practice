43eb635d5d7de2155fa8f63eac33782b2198203d
asadp10 <asadpathan01010@gmail.com>
2026-08-08 01:14:46 +0530

    .

--- 01_Patterns/pattern1.cpp
+++ 01_Patterns/pattern1.cpp
@@ -95,31 +95,34 @@
 
 //! Question 11 
 
-#include<iostream>
-using namespace std;
-int main(){
-    int n,i,d,j;
-    n=7;
-
-    for(i=0;i<n;i++){
-        for(j=0;j<=i;j++){
-            d=i-j;
-            if(d%2 == 0){
-                cout<<"1";
-            }else{
-                cout<<"0";
-            }
-
-        }
-    cout<<endl;
-    }
-}  
-
-
-// //!  Question 12
-
 // #include<iostream>
-// using namespace 
+// using namespace std;
+// int main(){
+//     int n,i,d,j;
+//     n=7;
+
+//     for(i=0;i<n;i++){
+//         for(j=0;j<=i;j++){
+//             d=i-j;
+//             if(d%2 == 0){
+//                 cout<<"1";
+//             }else{
+//                 cout<<"0";
+//             }
+
+//         }
+//     cout<<endl;
+//     }
+// }  
+
+
+//!  Question 12
+
+#include<iostream>
+using namespace std:
+int main(){
+
+}
 
 
 
