83fc6d5410f393cbbc4ae09f51e2f8abff313fea
asadp10 <asadpathan01010@gmail.com>
2026-08-09 22:59:32 +0530

    .

--- 02_Basic_Maths/07_Prime_no.cpp
+++ 02_Basic_Maths/07_Prime_no.cpp
@@ -2,14 +2,20 @@
 using namespace std;
 
 int main(){
-    int i, a, n = 36;
-    for(int i=1;i*i<=n;i++){
+    int i, a, n = 4;
+    int count = 0;
+    for(int i=2;i*i<=n;i++){
         if( n%i == 0){
-            cout<<i<<" ";
-            a=n/i;
-            cout<<a<<" ";
+            count++;
+            break;
         }
-    
+    }
+    if( n==1 ){
+        cout<<"Not Prime number";
+    }else if( count > 0 ){
+        cout<<"Not Prime number...";
+    }else{
+        cout<<"Prime Number...";
     }
     return 0;
 }
