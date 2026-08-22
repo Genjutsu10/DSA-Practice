e257bc304b44cbd2703b4bbc8ddfadfaa3f707b3
asadp10 <asadpathan01010@gmail.com>
2026-08-22 23:34:41 +0530

    .

---
+++ 06_Arrays/02_Medium/01_Two_Sum.cpp
@@ -0,0 +1,30 @@
+
+
+
+//! =========================================== Brute Force.... ===========================================  37
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        vector<int> res;
+
+        int n = nums.size();
+
+        for(int i = 0; i<n; i++){
+            for(int j = i+1; j<n; j++){
+                if(nums[i]+nums[j] == target){
+                    return {i,j};
+                }
+            }
+        }
+        return {};
+    }
+};
+
+
+
