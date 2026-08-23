7d5e4f38a0e5965150b8043ebb8d0949f67a6cc3
asadp10 <asadpathan01010@gmail.com>
2026-08-23 15:58:25 +0530

    .

--- 06_Arrays/02_Medium/03_Majority_Elements.cpp
+++ 06_Arrays/02_Medium/03_Majority_Elements.cpp
@@ -36,3 +36,32 @@ public:
 //! =========================================== OPTIMAL CODE... ===========================================
 
 
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        unordered_map<int,int> mp;
+
+        int n = nums.size();
+
+        int count = 0;
+        int candidate = 0;
+        for( int i=0; i<n; i++){
+
+            if( count == 0 ){
+                candidate = nums[i];
+            }
+
+            if( nums[i] == candidate){
+                count++; 
+            }
+            else{
+                count--;
+            }
+
+        }
+
+        return candidate;
+    }
+};
+
