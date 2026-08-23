0f0071735ab827507e0e49a3983ed35fc229ab51
asadp10 <asadpathan01010@gmail.com>
2026-08-23 16:06:49 +0530

    .

--- 06_Arrays/02_Medium/03_Majority_Elements.cpp
+++ 06_Arrays/02_Medium/03_Majority_Elements.cpp
@@ -49,7 +49,7 @@ public:
         for( int i=0; i<n; i++){
 
             if( count == 0 ){
-                candidate = nums[i];
+                candidate = nums[i];     
             }
 
             if( nums[i] == candidate){
@@ -65,3 +65,23 @@ public:
     }
 };
 
+
+/*
+This idea of candidate - count is crazy...
+
+pehle kya huua count 0 tha tb arr[i] ko candidate bna diya phir jb jb wo bnada repeat hoga,,
+tb tb (count ++) hoga lekin jaise he wo nhi milega (count --) hoga lekin candidate change nhi hoga...
+imagine koi candidate 5 baar mil gya to ab uska count = 5 hai, lekin ab es wali iteration mai nhi mila..
+that doesnt mean ki wo max nhi ho skta jb tk count -- ho krke 0 nhi krta tb tk wahi hoga cnadidate..
+
+aisa kyuuu ??
+q. mai diyahai ki majority matlab half of the array se bhi jyada count...
+? assume that the majority element always exists in the array. 
+agar exterem case bhi aa gyi to bhi wahi candidate hoga uper ki condition..
+means ki agar {1,1,1,2,2,2,1} aaya to bhi count 1 nhi ho payega...
+
+
+
+
+
+*/
